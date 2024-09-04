c791f032fed9b68a9fce6e1ee7897cc7b5464021
LyubovIv <iamlou@yandex.ru>
2024-09-04 19:59:19 +0300

    кастомизирована AdminUsersForm

--- clientChat/AdminUsersForm.Designer.cs
+++ clientChat/AdminUsersForm.Designer.cs
@@ -28,11 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminUsersForm));
             this.label2 = new System.Windows.Forms.Label();
-            this.buttonSaveUsers = new System.Windows.Forms.Button();
             this.DGVUsers = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.close = new CustomElements.CustomButton();
+            this.minimized = new CustomElements.CustomButton();
+            this.buttonSaveUsers = new CustomElements.CustomButton();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.DGVUsers)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
@@ -44,46 +49,116 @@
             this.label2.TabIndex = 20;
             this.label2.Text = "Активные пользователи";
             // 
-            // buttonSaveUsers
-            // 
-            this.buttonSaveUsers.Location = new System.Drawing.Point(594, 26);
-            this.buttonSaveUsers.Name = "buttonSaveUsers";
-            this.buttonSaveUsers.Size = new System.Drawing.Size(133, 56);
-            this.buttonSaveUsers.TabIndex = 19;
-            this.buttonSaveUsers.Text = "Сохранить изменения списка пользователей";
-            this.buttonSaveUsers.UseVisualStyleBackColor = true;
-            this.buttonSaveUsers.Click += new System.EventHandler(this.buttonSaveUsers_Click);
-            // 
             // DGVUsers
             // 
+            this.DGVUsers.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
             this.DGVUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVUsers.Location = new System.Drawing.Point(12, 26);
+            this.DGVUsers.Location = new System.Drawing.Point(1, 1);
             this.DGVUsers.Name = "DGVUsers";
-            this.DGVUsers.Size = new System.Drawing.Size(576, 154);
+            this.DGVUsers.Size = new System.Drawing.Size(574, 152);
             this.DGVUsers.TabIndex = 18;
+            this.DGVUsers.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.DGVUsers_RowPrePaint);
             // 
-            // label1
+            // label4
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 13);
-            this.label1.TabIndex = 21;
-            this.label1.Text = "Активные пользователи";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(48)))), ((int)(((byte)(68)))));
+            this.label4.Location = new System.Drawing.Point(7, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(221, 28);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Активные пользователи";
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.close.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.close.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
+            this.close.BorderRadius = 5;
+            this.close.BorderSize = 1;
+            this.close.FlatAppearance.BorderSize = 0;
+            this.close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close.ForeColor = System.Drawing.Color.White;
+            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
+            this.close.Location = new System.Drawing.Point(554, 12);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(34, 34);
+            this.close.TabIndex = 37;
+            this.close.TextColor = System.Drawing.Color.White;
+            this.close.UseVisualStyleBackColor = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // minimized
+            // 
+            this.minimized.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.minimized.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.minimized.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
+            this.minimized.BorderRadius = 5;
+            this.minimized.BorderSize = 1;
+            this.minimized.FlatAppearance.BorderSize = 0;
+            this.minimized.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
+            this.minimized.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimized.ForeColor = System.Drawing.Color.White;
+            this.minimized.Image = ((System.Drawing.Image)(resources.GetObject("minimized.Image")));
+            this.minimized.Location = new System.Drawing.Point(514, 12);
+            this.minimized.Name = "minimized";
+            this.minimized.Size = new System.Drawing.Size(34, 34);
+            this.minimized.TabIndex = 38;
+            this.minimized.TextColor = System.Drawing.Color.White;
+            this.minimized.UseVisualStyleBackColor = false;
+            this.minimized.Click += new System.EventHandler(this.minimized_Click);
+            // 
+            // buttonSaveUsers
+            // 
+            this.buttonSaveUsers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.buttonSaveUsers.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.buttonSaveUsers.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
+            this.buttonSaveUsers.BorderRadius = 5;
+            this.buttonSaveUsers.BorderSize = 1;
+            this.buttonSaveUsers.FlatAppearance.BorderSize = 0;
+            this.buttonSaveUsers.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
+            this.buttonSaveUsers.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
+            this.buttonSaveUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSaveUsers.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonSaveUsers.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(48)))), ((int)(((byte)(68)))));
+            this.buttonSaveUsers.Location = new System.Drawing.Point(148, 239);
+            this.buttonSaveUsers.Name = "buttonSaveUsers";
+            this.buttonSaveUsers.Size = new System.Drawing.Size(440, 45);
+            this.buttonSaveUsers.TabIndex = 39;
+            this.buttonSaveUsers.Text = "Сохранить изменения списка пользователей";
+            this.buttonSaveUsers.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(48)))), ((int)(((byte)(68)))));
+            this.buttonSaveUsers.UseVisualStyleBackColor = false;
+            this.buttonSaveUsers.Click += new System.EventHandler(this.buttonSaveUsers_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.DGVUsers);
+            this.panel1.Location = new System.Drawing.Point(12, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(576, 154);
+            this.panel1.TabIndex = 40;
             // 
             // AdminUsersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(742, 194);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(600, 297);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.buttonSaveUsers);
-            this.Controls.Add(this.DGVUsers);
+            this.Controls.Add(this.minimized);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AdminUsersForm";
             this.Text = "Книжный магазин (администратор)";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AdminUsersForm_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.DGVUsers)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,8 +167,11 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button buttonSaveUsers;
         private System.Windows.Forms.DataGridView DGVUsers;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private CustomElements.CustomButton close;
+        private CustomElements.CustomButton minimized;
+        private CustomElements.CustomButton buttonSaveUsers;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- clientChat/AdminUsersForm.cs
+++ clientChat/AdminUsersForm.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,9 +23,26 @@ namespace clientChat
         string sql = null;
         LoginForm loginForm = null;
 
+        //скругление формы
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+           int nLeftRect,
+           int nTopRect,
+           int nRightRect,
+           int nBottomRect,
+           int nWidthEllipse,
+           int nHeightEllipse
+        );
+
         public AdminUsersForm(LoginForm lg)
         {
             InitializeComponent();
+
+            //скругление формы
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+
             conn = new SqlConnection();
             cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
             conn.ConnectionString = cs;
@@ -71,6 +89,37 @@ namespace clientChat
             {
                 MessageBox.Show("Ошибка");
             }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimized_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void DGVUsers_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            this.DGVUsers.RowsDefaultCellStyle.BackColor = Color.FromArgb(189, 224, 254);
+            this.DGVUsers.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 247, 250);
+
         }
     }
 }
