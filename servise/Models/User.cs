86eba3a4efa540630d4618b5bf9da6050a209b77
Vladimir Ignatiev <akrololpwnz@gmail.com>
2024-09-05 01:05:19 +0400

    reference

---
+++ servise/Models/User.cs
@@ -0,0 +1,15 @@
+using System.ServiceModel;
+
+namespace service.Models
+
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public OperationContext opCon { get; set; }
+        public OperationContext OpCon { get; set; }
+
+
+    }
+}
---
+++ servise/Services/IChatService.cs
@@ -0,0 +1,23 @@
+using System.ServiceModel;
+
+namespace service.Services
+
+{
+    [ServiceContract(CallbackContract = typeof(IChatServiceCallback))]
+    public interface IChatService
+    {
+        [OperationContract]
+        int Connect(string name);
+        
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string msg, int id);
+    }
+    public interface IChatServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MessageCallback(string msg);
+    }
+}
