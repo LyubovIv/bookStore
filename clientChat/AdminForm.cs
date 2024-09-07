55e71fec744700f0be6413dc466b58e04d2844bd
Vladimir Ignatiev <akrololpwnz@gmail.com>
2024-09-07 15:55:36 +0400

    SCRUM-30 Добавил AdminFormHelper.cs; Произвел рефакторинг админки;

--- clientChat/AdminForm.cs
+++ clientChat/AdminForm.cs
@@ -1,111 +1,75 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace clientChat
 {
     public partial class AdminForm : Form
     {
-        SqlConnection conn;
-        string cs = null;
-        DataSet set = null;
-        SqlCommandBuilder cmdb = null;
-        SqlDataAdapter da = null;
-        string sql = null;
-        LoginForm loginForm = null;
-
-        //скругление формы
-        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
-        private static extern IntPtr CreateRoundRectRgn
-        (
-           int nLeftRect,
-           int nTopRect,
-           int nRightRect,
-           int nBottomRect,
-           int nWidthEllipse,
-           int nHeightEllipse
-        );
-
-        public AdminForm(LoginForm lg)
+        public AdminForm(LoginForm loginForm)
         {
             InitializeComponent();
 
             //скругление формы
-            this.FormBorderStyle = FormBorderStyle.None;
-            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            FormBorderStyle = FormBorderStyle.None;
+            Region = Region.FromHrgn(ClientApp.AdminFormHelper.CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
 
-            conn = new SqlConnection();
-            cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
-            conn.ConnectionString = cs;
+            ClientApp.AdminFormHelper.LoginFormDbConnection(loginForm);
 
-            loginForm = lg;
-
-            sql = "select * from [dbo].[Books]";
             FillDataGridView();
         }
+
         private void FillDataGridView()
         {
             try
             {
-                conn = new SqlConnection(cs);
-                set = new DataSet();
-                da = new SqlDataAdapter(sql, conn);
+                ClientApp.AdminFormHelper.BuildConfiguration();
                 DGVBooks.DataSource = null;
-                cmdb = new SqlCommandBuilder(da);
-                da.Fill(set);
-                DGVBooks.DataSource = set.Tables[0];
-                sql = null;
+                ClientApp.AdminFormHelper.DataAdapter.Fill(ClientApp.AdminFormHelper.DataSet);
+                DGVBooks.DataSource = ClientApp.AdminFormHelper.DataSet.Tables[0];
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Ошибка");
+                MessageBox.Show(@"Ошибка заполнения данных", ex.Message);
             }
         }
-
+        
         private void buttonSaveBooks_Click(object sender, EventArgs e)
         {
             try
             {
-                da.Update(set);
-                MessageBox.Show("Обновление успешно");
+                ClientApp.AdminFormHelper.DataAdapter.Update(ClientApp.AdminFormHelper.DataSet);
+                MessageBox.Show(@"Обновление успешно");
 
-                sql = null;
+                ClientApp.AdminFormHelper.Sql = null;
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Ошибка");
+                MessageBox.Show(@"Ошибка", ex.Message);
             }
         }
 
         private void AdminForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            loginForm.Close();
+            ClientApp.AdminFormHelper.LoginForm.Close();
         }
 
         private void DGVBooks_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
         {
-            this.DGVBooks.RowsDefaultCellStyle.BackColor = Color.FromArgb(189, 224, 254);
-            this.DGVBooks.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 247, 250);
+            DGVBooks.RowsDefaultCellStyle.BackColor = Color.FromArgb(189, 224, 254);
+            DGVBooks.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 247, 250);
         }
 
         private void close_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void minimized_Click(object sender, EventArgs e)
         {
-            this.WindowState = FormWindowState.Minimized;
+            WindowState = FormWindowState.Minimized;
         }
+
         protected override void WndProc(ref Message m)
         {
             switch (m.Msg)
