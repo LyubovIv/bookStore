a45c8f406348c71f6ab536b7b5d3082245d21166
Vladimir Ignatiev <akrololpwnz@gmail.com>
2024-09-07 15:55:36 +0400

    SCRUM-30 Добавил AdminFormHelper.cs; Произвел рефакторинг админки;

---
+++ clientChat/ClientApp/AdminFormHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+
+namespace clientChat.ClientApp
+{
+    public static class AdminFormHelper
+    {
+        
+        // ADO.NET запрос для получения списка книг
+        public static string Sql = "select * from [dbo].[Books]";
+        private static SqlConnection _connection;
+        private static string _cs = null;
+        public static DataSet DataSet = null;
+        public static SqlCommandBuilder CommandBuilder = null;
+        public static SqlDataAdapter DataAdapter = null;
+        public static LoginForm LoginForm = null;
+        private static string _getAllBooks = null;
+
+        public static void BuildConfiguration()
+        {
+            _connection = new SqlConnection(_cs);
+            DataSet = new DataSet();
+            DataAdapter = new SqlDataAdapter(Sql, _connection);
+            CommandBuilder = new SqlCommandBuilder(DataAdapter);
+        }
+
+        // Скругление формы
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRng")]
+        public static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+
+        public static void LoginFormDbConnection(LoginForm loginForm)
+        {
+            _connection = new SqlConnection();
+            _cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            _connection.ConnectionString = _cs;
+
+            LoginForm = loginForm;
+        }
+
+        
+        // Запасной метод
+        public static string GetAllBooksFromDb()
+        {
+            _getAllBooks = "select * from [dbo].[Books]";
+
+            return _getAllBooks;
+        }
+    }
+}
